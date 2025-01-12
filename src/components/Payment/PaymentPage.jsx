bc2a031b8866c85ef741de3dee1c6e48c647ec88
praveen378 <115167547+praveen378@users.noreply.github.com>
2025-01-12 21:50:32 +0530

    Update PaymentPage.jsx

--- src/components/Payment/PaymentPage.jsx
+++ src/components/Payment/PaymentPage.jsx
@@ -1,19 +1,15 @@
-import React, { useEffect, useState } from "react";
+  import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+
+import config from "../../config/config";
 
 const PaymentPage = () => {
   const location = useLocation();
-  const [amount, setAmount] = useState();
+
   const [qrLink, setQrLink] = useState("");
 
   useEffect(() => {
-    if (location.state && location.state.amount) {
-      setAmount(location.state.amount);
-      // setQrLink(` https://youtube.com`);
-      setQrLink(
-        `upi://pay?pa=praveen87552@okicici&pn=Praveen%20Rajput&am=${amount}.00&cu=INR&aid=uGICAgMDe_tz9ZQ`
-      );
-    }
+    setQrLink(config.paymentQrCoode);
   }, [location.state]);
   return (
     <div className="flex items-center justify-center h-screen bg-gray-200">
@@ -22,7 +18,7 @@ const PaymentPage = () => {
         <header className="bg-blue-300 text-white p-4 rounded-lg mb-4 flex justify-between items-center">
           <div className="    flex items-center justify-center text-black gap-4">
             Donate
-            <div className=""> ₹{amount}</div>
+            <div className=""> ₹51</div>
           </div>
         </header>
 
@@ -36,7 +32,6 @@ const PaymentPage = () => {
             {/* SCAN Option */}
             <Link
               to="/scanner"
-              state={{ amount }}
               className="flex justify-between items-center p-4 bg-white rounded-lg cursor-pointer hover:bg-gray-200"
             >
               <div className="flex items-center space-x-4">
@@ -46,7 +41,7 @@ const PaymentPage = () => {
               <div className=" bg-white rounded-full w-10 h-10 flex items-center justify-center     z-30 overflow-hidden">
                 <img
                   className="w-10"
-                  src="https://raw.githubusercontent.com/praveen378/everything-ok/refs/heads/main/src/assets/images/bar.webp" // Replace with real payment icons
+                  src="src/assets/images/bar.webp" // Replace with real payment icons
                   alt="UPI Options"
                 />
               </div>
@@ -55,7 +50,6 @@ const PaymentPage = () => {
             {/* UPI Option */}
             <a
               href={qrLink}
-              target="_blank"
               className="flex justify-between items-center p-4 bg-white rounded-lg cursor-pointer hover:bg-gray-200"
             >
               <div className="flex items-center space-x-4">
@@ -66,21 +60,21 @@ const PaymentPage = () => {
                 <div className="   bg-white rounded-full w-10 h-10 flex items-center justify-center border-2 border-gray-300 z-30 overflow-hidden">
                   <img
                     className="w-8 bg-white"
-                    src="https://raw.githubusercontent.com/praveen378/everything-ok/refs/heads/main/src/assets/images/phone.webp" // Replace with card icons
+                    src="src/assets/images/phone.webp" // Replace with card icons
                     alt="Card Options"
                   />
                 </div>
                 <div className="   bg-white rounded-full w-10 h-10 flex items-center justify-center border-2 border-gray-300 z-30 overflow-hidden">
                   <img
                     className="w-8"
-                    src="https://raw.githubusercontent.com/praveen378/everything-ok/refs/heads/main/src/assets/images/google.webp" // Replace with card icons
+                    src="src/assets/images/google.webp" // Replace with card icons
                     alt="Card Options"
                   />
                 </div>
                 <div className="   bg-white rounded-full w-10 h-10 flex items-center justify-center border-2 border-gray-300 z-30 overflow-hidden">
                   <img
                     className="w-9"
-                    src="https://raw.githubusercontent.com/praveen378/everything-ok/refs/heads/main/src/assets/images/patym.webp" // Replace with card icons
+                    src="src/assets/images/patym.webp" // Replace with card icons
                     alt="Card Options"
                   />
                 </div>
